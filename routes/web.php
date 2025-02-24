b8b671da18dff4feec3f4de90899c68580516973
wnsnilesh <nilesh.webnsoftsolution.com>
2025-02-24 11:23:37 +0530

    adding fav icon and fixing logo fixing login screen

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\Web\Admin\{
 };
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
