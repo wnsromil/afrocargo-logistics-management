7df00081bb272068f57adcce8a7823ee13b0b2a2
Romil S <124246667+wnsromil@users.noreply.github.com>
2025-03-11 15:20:53 +0530

    working with api

--- app/Http/Controllers/Api/InvoiceController.php
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -29,25 +29,24 @@ class InvoiceController extends Controller
                 return $q->where('id', $user->warehouse_id);
             })->get();
 
-            $users = User::when($user->role_id != 1, function ($q) use ($user) {
-                return $q->where('warehouse_id', $user->warehouse_id);
-            })->get();
+            // $users = User::when($user->role_id != 1, function ($q) use ($user) {
+            //     return $q->where('warehouse_id', $user->warehouse_id);
+            // })->get();
 
-            $customers = $users->where('role_id', 3)->values();
+            // $customers = $users->where('role_id', 3)->values();
+            // $drivers = $users->where('role_id', 4)->values();
 
-            $drivers = $users->where('role_id', 4)->values();
-
-            $parcelTypes = Category::get();
+            // $parcelTypes = Category::get();
             $inventaries = Inventory::get();
-            $containers = Vehicle::where('vehicle_type','Container')->get();
+            $containers = Vehicle::where('vehicle_type', 'Container')->get();
 
             return response()->json([
                 'status' => 'success',
                 'user' => $user,
                 'warehouses' => $warehouses,
-                'customers' => $customers,
-                'drivers' => $drivers,
-                'parcelTypes' => $parcelTypes,
+                // 'customers' => $customers,
+                // 'drivers' => $drivers,
+                // 'parcelTypes' => $parcelTypes,
                 'inventaries' => $inventaries,
                 'containers' => $containers,
             ]);
@@ -67,32 +66,27 @@ class InvoiceController extends Controller
 
             $validatedData = $request->validate([
                 'customer_id' => 'required|exists:users,id',
-                'driver_id' => 'nullable|exists:users,id',
-                'warehouse_id' => 'required|exists:warehouses,id',
+                'shipping_user_id' => 'required',
                 'category_id' => 'required|exists:categories,id',
-                'inventory_id' => 'required|exists:inventories,id',
-                'weight' => 'required|numeric|min:0',
-                'price' => 'required|numeric|min:0',
-                'status' => 'required|in:pending,shipped,delivered',
+                'total_price' => 'required|numeric',
+                'inventory_ids.*' => 'exists:inventories,id',
             ]);
 
-            // Store new parcel
-            $parcel = Invoice::create([
+            // Create New Invoice
+            $invice = Invoice::create([
                 'customer_id' => $validatedData['customer_id'],
-                'driver_id' => $validatedData['driver_id'] ?? null,
-                'warehouse_id' => $validatedData['warehouse_id'],
+                'shipping_user_id' => $validatedData['shipping_user_id'],
                 'category_id' => $validatedData['category_id'],
-                'inventory_id' => $validatedData['inventory_id'],
-                'weight' => $validatedData['weight'],
-                'price' => $validatedData['price'],
+                'total_price' => $validatedData['total_price'],
                 'status' => $validatedData['status'],
                 'created_by' => $user->id,
+                'inventory_ids' => $validatedData['inventory_ids'],
             ]);
 
             return response()->json([
                 'status' => 'success',
-                'message' => 'Parcel created successfully!',
-                'parcel' => $parcel
+                'message' => 'Invoice created successfully!',
+                'invice' => $invice
             ], 201);
         } catch (\Throwable $th) {
             return response()->json([
@@ -102,5 +96,12 @@ class InvoiceController extends Controller
             ], 500);
         }
     }
-
+    public function inventaries()
+    {
+        $inventaries = Inventory::where('status', 'Active')->get();
+        return response()->json([
+            'status' => 'success',
+            'inventaries' => $inventaries
+        ], 200);
+    }
 }
