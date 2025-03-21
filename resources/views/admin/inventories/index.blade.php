377755a71f87051b8d57b7132a25bc6ae2e21261
sandeepwns <sandeeppwns@gmail.com>
2025-03-21 15:30:31 +0530

    changes code and update

--- resources/views/admin/inventories/index.blade.php
+++ resources/views/admin/inventories/index.blade.php
@@ -128,7 +128,7 @@
                                                         href="{{ route('admin.inventories.edit', $inventory->id) }}"><i
                                                             class="far fa-edit me-2"></i>Edit</a>
                                                 </li>
-                                                <li>
+                                                {{-- <li>
 
                                                     <form
                                                         action="{{ route('admin.inventories.destroy', $inventory->id) }}"
@@ -139,7 +139,7 @@
                                                             onclick="deleteData(this,'Wait! 🤔 Are you sure you want to remove this inventory? This action can’t be undone! 🚀')"><i
                                                                 class="far fa-trash-alt me-2"></i>Delete</button>
                                                     </form>
-                                                </li>
+                                                </li> --}}
                                                 <li>
                                                     <a class="dropdown-item"
                                                         href="{{ route('admin.inventories.show', $inventory->id) }}"><i
