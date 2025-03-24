c265f017ca83cec873575ba272f293ae98db1d81
wnsSakshi <pooja@webnsoftsolution.com>
2025-03-24 16:46:23 +0530

    changes

--- resources/views/admin/customer/create.blade.php
+++ resources/views/admin/customer/create.blade.php
@@ -387,15 +387,15 @@
                     <div class="input-block mb-3">
                         <label class="foncolor" for="status">Status</label>
 
-                        <div class="status-toggle">
-                            <span>Active</span>
-                            <input id="status" class="check" type="checkbox" name="status" checked>
-                            <label for="status" class="checktoggle checkbox-bg togc"></label>
-                            <span class="">Inactive</span>
+                        <div class="d-flex align-items-center">
+                            <p class="profileUpdateFont" id="activeText">Active</p>
+                            <div class="status-toggle px-2">
+                                <input id="rating_6" class="check" type="checkbox" value="Inactive">
+                                <label for="rating_6" class="checktoggle log checkbox-bg">checkbox</label>
+                            </div>
+                            <p class="profileUpdateFont faded" id="inactiveText">Inactive</p>
                         </div>
-                        @error('status')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
+
 
                     </div>
                 
