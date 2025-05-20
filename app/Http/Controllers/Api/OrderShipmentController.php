658975d354188d277af62bb51f185611bea4f582
sandeepwns <sandeeppwns@gmail.com>
2025-05-20 18:46:47 +0530

    Add delivery and pickup type fields to Parcel model and store method

--- app/Http/Controllers/Api/OrderShipmentController.php
+++ app/Http/Controllers/Api/OrderShipmentController.php
@@ -84,6 +84,7 @@ class OrderShipmentController extends Controller
                 'pickup_address_id' => 'required|numeric',
                 'delivery_address_id' => 'required|numeric',
                 'pickup_time' => 'required|string',
+                'delivery_type' => 'required|string',
                 'pickup_date' => 'required|date',
                 'source_address' => 'required',
             ]);
